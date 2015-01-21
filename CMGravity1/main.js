786dda7a4c8c521f83949b5d35ed4245014e91b4
Ryan Malm <ryan.malm@gmail.com>
2015-01-21 15:42:43 +0000

    couple more exercises complete, functions added to utils.js

--- CMGravity1/main.js
+++ CMGravity1/main.js
@@ -4,10 +4,10 @@ window.onload = function() {
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight,
         
-        sun = particle.create(width / 2, height / 2, 0,0),
-        planet = particle.create(width /2 + 200, height / 2, 10, Math.PI * 2);
+        sun = particle.create(width / 2, height / 2, 0, 0),
+        planet = particle.create(width /2 + 200, height / 2, 10, -Math.PI / 2);
         
-    sun.mass = 20000;
+    sun.mass = 24000;
 
     update();
     
@@ -18,15 +18,14 @@ window.onload = function() {
         planet.update();
         
         context.beginPath();
-        context.fillStyle = "#FFFF00";
+        context.fillStyle = "#FFEE00";
         context.arc(sun.position.getX(), sun.position.getY(), 20, 0, Math.PI * 2, false);
-        context.fill;
+        context.fill();
         
         context.beginPath();
         context.fillStyle = "#0000FF";
         context.arc(planet.position.getX(), planet.position.getY(), 10, 0, Math.PI * 2, false);
-        context.fill;
-)
+        context.fill();
         
     //animation code goes here
         
